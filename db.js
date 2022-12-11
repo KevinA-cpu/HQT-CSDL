5ff5bac0711eaf18b849448de776ec4cfa072b80
KevinA-cpu <kevinanderson1410@gmail.com>
2022-12-11 09:52:43 +0700

    #add lost update

--- db.js
+++ db.js
@@ -1,20 +1,12 @@
 const config = {
-    user: "sa",
-    password: "datphan",
-    server: "DATPHAN",
-    options: {
-        port: 1433,
-        encrypt: false,
-        database: "GiaoDoAnABC",
-        // connectTimeout: 20000,
-        // requestTimeout: 150000,
-        enableArithAbort: true,
-    },
-    // pool: {
-    //     max: 10,
-    //     min: 5,
-    //     idleTimeoutMillis: 3000,
-    // },
+  user: "dhlogin",
+  password: "seaways456",
+  server: "LAPTOP-TGVBBNN7",
+  options: {
+    port: 1433,
+    encrypt: false,
+    database: "GiaoDoAnABC",
+  },
 };
 
 export default config;
--- src/lostupdate/controller.js
+++ src/lostupdate/controller.js
@@ -3,133 +3,130 @@ import queries from "./queries.js";
 import sql from "mssql";
 
 const T1 = async (req, res) => {
-    let isolationLevel = sql.ISOLATION_LEVEL.READ_COMMITTED;
+  let isolationLevel = sql.ISOLATION_LEVEL.READ_COMMITTED;
+  try {
+    let pool = await sql.connect(config);
+    let transaction = new sql.Transaction(pool);
     try {
-        let pool = await sql.connect(config);
-        let transaction = new sql.Transaction(pool);
-        try {
-            await transaction.begin(isolationLevel);
-            let like = await transaction.request().query(queries.setLuotLike);
-            await transaction.request().query(queries.delay);
-            like.recordset[0].LuotLike += 300; // Tang luot like them 300
+      await transaction.begin(isolationLevel);
+      let like = await transaction.request().query(queries.setLuotLike);
+      await transaction.request().query(queries.delay);
+      console.log(like.recordset);
+      like.recordset[0].LuotLike += 300; // Tang luot like them 300
 
-            await transaction.request()
-                .query`UPDATE MonAn SET LuotLike = ${like.recordset[0].LuotLike} WHERE TenMon = N'Cơm hến'`;
+      await transaction.request()
+        .query`UPDATE MonAn SET LuotLike = ${like.recordset[0].LuotLike} WHERE TenMon = N'Cơm hến'`;
 
-            let data = await transaction.request().query(queries.queryComHen);
+      let data = await transaction.request().query(queries.queryComHen);
 
-            await transaction.commit();
-            res.status(200).send(data.recordsets);
-        } catch (error) {
-            await transaction.rollback();
-            throw error;
-        } finally {
-            await pool.close();
-        }
+      await transaction.commit();
+      res.status(200).send(data.recordsets);
     } catch (error) {
-        throw error;
+      await transaction.rollback();
+      throw error;
+    } finally {
+      await pool.close();
     }
+  } catch (error) {
+    throw error;
+  }
 };
 
 const T2 = async (req, res) => {
-    let isolationLevel = sql.ISOLATION_LEVEL.SERIALIZABLE;
+  let isolationLevel = sql.ISOLATION_LEVEL.SERIALIZABLE;
 
+  try {
+    let pool = await sql.connect(config);
+    let transaction = new sql.Transaction(pool);
     try {
-        let pool = await sql.connect(config);
-        let transaction = new sql.Transaction(pool);
-        try {
-            await transaction.begin(isolationLevel);
-            let like = await transaction.request().query(queries.setLuotLike);
-            like.recordset[0].LuotLike -= 100; // Giam luot like di 100
+      await transaction.begin(isolationLevel);
+      let like = await transaction.request().query(queries.setLuotLike);
+      like.recordset[0].LuotLike -= 100; // Giam luot like di 100
 
-            await transaction.request()
-                .query`UPDATE MonAn SET LuotLike = ${like.recordset[0].LuotLike} WHERE TenMon = N'Cơm hến'`;
+      await transaction.request()
+        .query`UPDATE MonAn SET LuotLike = ${like.recordset[0].LuotLike} WHERE TenMon = N'Cơm hến'`;
 
-            let data = await transaction.request().query(queries.queryComHen);
+      let data = await transaction.request().query(queries.queryComHen);
 
-            await transaction.commit();
-            res.status(200).send(data.recordsets);
-        } catch (error) {
-            await transaction.rollback();
-            throw error;
-        } finally {
-            await pool.close();
-        }
+      await transaction.commit();
+      res.status(200).send(data.recordsets);
     } catch (error) {
-        throw error;
+      await transaction.rollback();
+      throw error;
+    } finally {
+      await pool.close();
     }
+  } catch (error) {
+    throw error;
+  }
 };
 
 const T3 = async (req, res) => {
-    let isolationLevel = req.params.isolationlevel;
-    if (isolationLevel === "SERIALIZABLE")
-        isolationLevel = sql.ISOLATION_LEVEL.SERIALIZABLE;
+  let isolationLevel = req.params.isolationlevel;
+  if (isolationLevel === "SERIALIZABLE")
+    isolationLevel = sql.ISOLATION_LEVEL.SERIALIZABLE;
 
+  try {
+    let pool = await sql.connect(config);
+    let transaction = new sql.Transaction(pool);
     try {
-        let pool = await sql.connect(config);
-        let transaction = new sql.Transaction(pool);
-        try {
-            await transaction.begin(isolationLevel);
-            let like = await transaction
-                .request()
-                .query(queries.setLuotLike_XLock);
-            await transaction.request().query(queries.delay);
-            like.recordset[0].LuotLike += 300; // Tang luot like them 300
+      await transaction.begin(isolationLevel);
+      let like = await transaction.request().query(queries.setLuotLike_XLock);
+      await transaction.request().query(queries.delay);
+      like.recordset[0].LuotLike += 300; // Tang luot like them 300
 
-            await transaction.request()
-                .query`UPDATE MonAn with (xlock) SET LuotLike = ${like.recordset[0].LuotLike} WHERE TenMon = N'Cơm hến'`;
+      await transaction.request()
+        .query`UPDATE MonAn with (xlock) SET LuotLike = ${like.recordset[0].LuotLike} WHERE TenMon = N'Cơm hến'`;
 
-            let data = await transaction.request().query(queries.queryComHen);
+      let data = await transaction.request().query(queries.queryComHen);
 
-            await transaction.commit();
-            res.status(200).send(data.recordsets);
-        } catch (error) {
-            await transaction.rollback();
-            throw error;
-        } finally {
-            await pool.close();
-        }
+      await transaction.commit();
+      res.status(200).send(data.recordsets);
     } catch (error) {
-        throw error;
+      await transaction.rollback();
+      throw error;
+    } finally {
+      await pool.close();
     }
+  } catch (error) {
+    throw error;
+  }
 };
 
 const T4 = async (req, res) => {
-    let isolationLevel = req.params.isolationlevel;
-    if (isolationLevel === "SERIALIZABLE")
-        isolationLevel = sql.ISOLATION_LEVEL.SERIALIZABLE;
+  let isolationLevel = req.params.isolationlevel;
+  if (isolationLevel === "SERIALIZABLE")
+    isolationLevel = sql.ISOLATION_LEVEL.SERIALIZABLE;
 
+  try {
+    let pool = await sql.connect(config);
+    let transaction = new sql.Transaction(pool);
     try {
-        let pool = await sql.connect(config);
-        let transaction = new sql.Transaction(pool);
-        try {
-            await transaction.begin(isolationLevel);
-            let like = await transaction
-                .request()
-                .query(queries.setLuotLike_XLock);
-            like.recordset[0].LuotLike -= 100; // Giam luot like di 100
+      await transaction.begin(isolationLevel);
+      let like = await transaction.request().query(queries.setLuotLike_XLock);
+      like.recordset[0].LuotLike -= 100; // Giam luot like di 100
 
-            await transaction.request()
-                .query`UPDATE MonAn with (xlock) SET LuotLike = ${like.recordset[0].LuotLike} WHERE TenMon = N'Cơm hến'`;
+      await transaction.request()
+        .query`UPDATE MonAn with (xlock) SET LuotLike = ${like.recordset[0].LuotLike} WHERE TenMon = N'Cơm hến'`;
 
-            let data = await transaction.request().query(queries.queryComHen);
+      let data = await transaction.request().query(queries.queryComHen);
 
-            await transaction.commit();
-            res.status(200).send(data.recordsets);
-        } catch (error) {
-            await transaction.rollback();
-            throw error;
-        } finally {
-            await pool.close();
-        }
+      await transaction.commit();
+      res.status(200).send(data.recordsets);
     } catch (error) {
-        throw error;
+      await transaction.rollback();
+      throw error;
+    } finally {
+      await pool.close();
     }
+  } catch (error) {
+    throw error;
+  }
 };
 
 export default {
-    T1,
-    T2,
-    T3,
-    T4,
+  T1,
+  T2,
+  T3,
+  T4,
 };
